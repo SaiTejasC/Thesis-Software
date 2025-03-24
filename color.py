499abb2bd727a51d2ee012c628b764d92c870c67
Sai Chintapalli <sai.chintapalli@epfl.ch>
2025-03-24 17:30:02 +0100

    started working on color detection

--- color.py
+++ color.py
@@ -1,1 +1,14 @@
 import cv2 as cv
+import numpy as np
+
+def detect_red(image):
+    hsvImage = cv.cvtColor(image,cv.COLOR_BGR2HSV)
+    lower1 = np.array([0, 100, 150])
+    upper1 = np.array([10, 255, 255])
+    lower2 = np.array([160, 100, 150])
+    upper2 = np.array([180, 255, 255])
+    
+    mask1 = cv.inRange(hsvImage, lower1 ,upper1)
+    mask2 = cv.inRange(hsvImage, lower2 ,upper2)
+    mask = mask1 + mask2
+    return mask
--- main.py
+++ main.py
@@ -1,2 +1,16 @@
 import cv2 as cv
+from color import *
+cam = cv.VideoCapture(0)
+while True:
+    ret, frame = cam.read()
+    frame2 = detect_red(frame)
+    cv.imshow('frame2', frame2)
+    cv.imshow('frame', frame)
 
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cam.release()
+cv.destroyAllWindows()
+
+detect_red(1)
